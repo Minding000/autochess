e4c443e298690dbee7ef115bd19efbaf22863a70
john <johnathan.stephens@protonmail.com>
2023-11-30 16:49:49 +0100

    add boardstate and some clean-up

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,65 +1,40 @@
 extends Node2D
 
-const red_piece_scene = preload("res://scenes/pieces/red_piece.tscn")
-
-const STARTING_VEC = Vector2(64, 64)*2
-const STARTING_VEC_CENTER = Vector2(64, 64)*3
+const FIRST_TILE_POSITION = Vector2(64, 64)*2
+const FIRST_TILE_CENTER = Vector2(64, 64)*3
 const STEP = 64*2
 
+const ORIGINAL_TILE_SIZE = 64
 var TILEMAP_SCALE = 0
-var ORIGINAL_TILE_SIZE = 0
 var EFFECTIVE_TILE_SIZE = 0
 var BOARD_OFFSET = 0
 
+const ROW_COUNT = 8
+const COLUMN_COUNT = 8
 
-# Called when the node enters the scene tree for the first time.
+var board = []
+
+
 func _ready():
-	$"Red-2".position = STARTING_VEC_CENTER
-	
 	TILEMAP_SCALE = $Tiles.scale
-	ORIGINAL_TILE_SIZE = 64
 	EFFECTIVE_TILE_SIZE = ORIGINAL_TILE_SIZE * TILEMAP_SCALE
 	BOARD_OFFSET = EFFECTIVE_TILE_SIZE
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	input_shenanigans()
-
-
-func input_shenanigans():
-	if Input.is_action_just_pressed("primary action"):
-		$"Red-2".position.x += EFFECTIVE_TILE_SIZE.x
 	
-	if Input.is_action_just_pressed("secondary action"):
-		$"Red-2".position.y += EFFECTIVE_TILE_SIZE.y
+	for _i in range(ROW_COUNT):
+		var column = []
+		column.resize(COLUMN_COUNT)
+		board.append(column)
 
 
-func get_vec_to_center_of_tile(row, col) -> Vector2:
-	if Vector2(0, 0) <= Vector2(row, col) and Vector2(row, col) <= Vector2(7, 7): 
-		return STARTING_VEC_CENTER + Vector2(row, col)*EFFECTIVE_TILE_SIZE
-	else:
-		# crash game ftw
-		return STARTING_VEC_CENTER
-	
-
-func get_col_row(pos: Vector2):
-	var col_row_vec = ((pos - STARTING_VEC) / EFFECTIVE_TILE_SIZE)
-	
-	return Vector2(int(col_row_vec.x), int(col_row_vec.y))
-	
-#	return Vector2(int((pos.x - BOARD_OFFSET) / EFFECTIVE_TILE_SIZE), int((pos.y - BOARD_OFFSET) / EFFECTIVE_TILE_SIZE))
+func get_vector_to_center_of_tile(column, row) -> Vector2:
+	assert(
+		Vector2(0, 0) <= Vector2(column, row) and
+		Vector2(column, row) < Vector2(COLUMN_COUNT, ROW_COUNT),
+		'Position (column %d | row %d) is out of bounds.' % [column, row]
+	)
+	return FIRST_TILE_CENTER + Vector2(column, row)*EFFECTIVE_TILE_SIZE
 
 
-	
-func todo_yes(instructions):
-	# save stationed unit in each tile
-	# instantiate with none
-
-	# add move unit while holding
-	# on unit drop:
-		# check corresponding tile
-		# update tile info with new unit
-		# swap if existent
-	
-	print('just DO iT:1')
+func get_column_row_position(pixel_position: Vector2):
+	var column_row_position = ((pixel_position - FIRST_TILE_POSITION) / EFFECTIVE_TILE_SIZE)
+	return Vector2(int(column_row_position.x), int(column_row_position.y))
